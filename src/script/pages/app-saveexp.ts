6c7005708685ec7c8e5597509d41a1e143d35708
Lithius <aerosiiap@gmail.com>
2021-11-06 20:16:29 -0400

    accepts

--- src/script/pages/app-saveexp.ts
+++ src/script/pages/app-saveexp.ts
@@ -1,8 +1,10 @@
 import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
+import { Recipe } from '../components/recipe';
 
-@customElement('app-test')
+@customElement('app-saveexp')
 export class AppSaveTest extends LitElement {
+
   // For more information on using properties and state in lit
   // check out this link https://lit.dev/docs/components/properties/
 
@@ -10,12 +12,43 @@ export class AppSaveTest extends LitElement {
     super();
   }
 
-  async firstUpdated() {
-    // this method is a lifecycle even in lit
-    // for more info check out the lit docs https://lit.dev/docs/components/lifecycle/
+  textToSave: string = "";
+
+  onSaveButtonClick() {
+    let testRecipe : Recipe = new Recipe();
+    testRecipe.title = "banan";
+    testRecipe.ingredients = "potassium";
+    testRecipe.directions = "eat banan";
+
+    testRecipe.saveToStorage();
+    console.log("banan has been saved");
   }
 
+  onLoadButtonClick() {
+    let loadedRecipe: Recipe | null = Recipe.loadFromStorage("banan");
+
+    if (loadedRecipe) {
+
+      let sr: ShadowRoot | null = this.shadowRoot;
+      if (sr) {
+        let loadText = ((sr) as ShadowRoot).getElementById("loadText");
+
+        if (loadText) {
+          let testString: string = "<p>" + loadedRecipe.title + "</p>"
+            + "<p>" + loadedRecipe.ingredients + "</p>"
+            + "<p>" + loadedRecipe.directions + "</p>";
+
+          (loadText as HTMLElement).innerHTML = testString;
+        }
+      }
+    } else {
+      console.log("Loading Failed");
+    }
+  }
+
+
   render() {
+
     return html`
       <app-header enableBack="${true}"></app-header>
 
@@ -23,9 +56,20 @@ export class AppSaveTest extends LitElement {
         <h2>
           Minimalist test page for testing saving and loading.
         </h2>
+      </div>
+      <div>
         <p>
-          thing
+          Type something down below:
         </p>
+
+        <p>
+          <button @click=${this.onSaveButtonClick}>Save</button>
+        </p>
+        <p>
+          <button @click=${this.onLoadButtonClick}>Load</button>
+          <div id="loadText"></div>
+        </p>
+
       </div>
     `;
   }
